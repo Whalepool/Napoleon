e4aa25a78f4f2713be3346491d21acd93cfd11c2
flibbr <flibbr@gmail.com>
2018-06-10 00:45:17 +0100

    Added up to date checker

---
+++ uptodate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+
+git remote -v update
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
